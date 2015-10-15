7bd1bacfbfdfeed48e708757d773156054941a9d
Guillaume Alpe <guillaumealpe@hotmail.com>
2015-10-16 02:23:34 +0530

    Interactive Nav

---
+++ scripts/nav.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".menu").hide();
+
+    $(function () {
+
+        $page = location.pathname.substr(location.pathname.lastIndexOf("/") + 1);
+        if (!$page) {
+            $page = 'index.html';
+        }
+        console.log($page);
+        $('menu, li, a').each(function () {
+            var $href = $(this).attr('href');
+            if (($href == $page) || ($href == '')) {
+                console.log($(this).attr('href'));
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
+
+});
+
+function showMenu() {
+    $(".menu").toggle();
+}
+
+//function changeActive() {
+//    $(".menu, li").removeClass("active");
+//    console.log("test");
+//    $("li", this).addClass("active");
+//}
