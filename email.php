854ab9efe89ff2540e5e2df9d32ee6f27060e882
Guillaume Alpe <guillaumealpe@hotmail.com>
2015-10-15 21:23:59 +0530

    email bug fix

--- email.php
+++ email.php
@@ -1,13 +1,14 @@
 <?php
-    $name = $_POST['name'];
+    $subject = $_POST['subject'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
 
     $email_from = 'gllm.lp.17@gmail.com';
     $email_subject = "New Form submission";
-    $email_body = "You have received a new message from the user $name.\n Here is the message:\n $message";
-        
+    $email_body = "You have received a new message from the user $subject.\n".
+                            "Here is the message:\n $message";
+
     $to = "gllm.lp.17@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
