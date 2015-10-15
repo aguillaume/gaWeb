faf59ff56bca63a14262bcc13efd3292ff96bf2a
unknown <wl73@UKLTD18640.global.tesco.org>
2015-10-15 16:46:36 +0530

    clearfix & HTML5 tags

--- email.php
+++ email.php
@@ -6,9 +6,8 @@
 
     $email_from = 'gllm.lp.17@gmail.com';
     $email_subject = "New Form submission";
-    $email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
-
+    $email_body = "You have received a new message from the user $name.\n Here is the message:\n $message";
+        
     $to = "gllm.lp.17@gmail.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
